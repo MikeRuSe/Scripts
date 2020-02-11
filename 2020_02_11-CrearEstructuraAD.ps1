f433e412b32f6cb0886a038a341f0d58bc22b0ec
MikeRuSe <57407059+MikeRuSe@users.noreply.github.com>
2020-02-11 17:50:14 +0100

    Update 2020_02_11-CrearEstructuraAD.ps1

--- 2020_02_11-CrearEstructuraAD.ps1
+++ 2020_02_11-CrearEstructuraAD.ps1
@@ -1,11 +1,11 @@
 Write-Host "Introduzca a que OU pertenecerá" -ForegroundColor Magenta
-$ou=Read-Host "OU"
+ $ou=Read-Host "OU"
 Write-Host "Introduzca a que grupo pertenecerá" -ForegroundColor Magenta
-$group=Read-Host "Grupo"
+ $group=Read-Host "Grupo"
 Write-Host "Introduzca el nombre de usuario" -ForegroundColor Magenta
-$user=Read-Host "Usuario"
+ $user=Read-Host "Usuario"
 Write-Host "Introduzca la contraseña 'tipo 8 carácteres con letras, numeros y caracteres especiales'" -ForegroundColor Magenta
-$pass=Read-Host "Contraseña"
+ $pass=Read-Host "Contraseña"
 "$ou,$group,$user,$pass" >> crear.csv
 
 
@@ -15,14 +15,24 @@ foreach($vars in (gc crear.csv)){
     $usuario= $vars.split(",")[2]
     $contraseña= $vars.split(",")[3]
     Clear
-New-ADOrganizationalUnit -Name $OU -ProtectedFromAccidentalDeletion $false -Path "DC=Andel,DC=local"
+  New-ADOrganizationalUnit -Name $OU -ProtectedFromAccidentalDeletion $false -Path "DC=Andel,DC=local"
+    Write-Host "Creando OU    " -ForegroundColor Cyan -NoNewline; Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green
+    Start-Sleep -Milliseconds 1350
     Clear
-    Write-Host "Creando OU    " -ForegroundColor Yellow -NoNewline; Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 80; Write-Host "." -ForegroundColor Green -NoNewline; Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 100; Write-Host "." -ForegroundColor Green -NoNewline; Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 100; Write-Host "." -ForegroundColor Green -NoNewline; Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 80; Write-Host "." -ForegroundColor Green -NoNewline; Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 100; Write-Host "." -ForegroundColor Green -NoNewline; Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 100; Write-Host "." -ForegroundColor Green -NoNewline; Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 100; Write-Host "." -ForegroundColor Green -NoNewline;
-New-ADuser -name $vars.split(",")[2] -sam $vars.split(",")[2] -accountpassword (convertto-securestring $contraseña -asplaintext -force) -enable $true
+  New-ADuser -name $usuario -sam $usuario -accountpassword (convertto-securestring $contraseña -asplaintext -force) -enable $true
+    Write-Host "Creando Usuario $usuario    " -ForegroundColor Cyan -NoNewline; Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green 
     Write-Host "Usuario '$usuario' creado " -ForegroundColor Green
-New-ADGroup -name $grupo -GroupScope Global -Path "OU=$OU,DC=Andel,DC=local"
+    Start-Sleep -Milliseconds 1350
+    Clear
+  New-ADGroup -name $grupo -GroupScope Global -Path "OU=$OU,DC=Andel,DC=local"
+    Write-Host "Creando Grupo $grupo    " -ForegroundColor Cyan -NoNewline; Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green   
     Write-Host "Grupo '$grupo' creado " -ForegroundColor Green
-Add-ADGroupMember $vars.split(",")[1] -Members $vars.split(",")[2]
-    Write-Host "Usuario '$usuario' se introdujo dentro del grupo '$grupo' " -ForegroundColor Yellow
+    Start-Sleep -Milliseconds 1350
+    Clear
+  Add-ADGroupMember $grupo -Members $usuario
+    Write-Host "Añadiendo el Usuario $usuario al grupo $grupo    " -ForegroundColor Cyan -NoNewline; Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green -NoNewline; Start-Sleep -Milliseconds 150;Write-Host "." -ForegroundColor Green
+    Write-Host "Usuario '$usuario' se introdujo dentro del grupo '$grupo' " -ForegroundColor Green
+    Clear
     }
-    Start-Sleep -Milliseconds 100
+Start-Sleep -Milliseconds 100; Write-Host "SE EJECUTARON TODOS LOS SCRIPTS DE LA SECUENCIA 'FOREACH' " -ForegroundColor Yellow
+    
